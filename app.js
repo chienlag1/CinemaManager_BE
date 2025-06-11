80a3febf5abf0e3b05aa969ef0ec4762ab85c44f
Chienlag1 <chienguyen17@gmail.com>
2025-06-11 17:07:08 +0700

    [MovieTicket7] add app

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const movieRouter = require('./src/routes/movieRoutes');
 const authRouter = require('./src/routes/authRoutes');
+
+const roomRoutes = require('./src/routes/roomRoutes'); // Thêm roomRoutes
+const showtimeRoutes = require('./src/routes/showTimeRoutes'); // Thêm showtimeRoutes
+const ticketRoutes = require('./src/routes/ticketRoutes'); // Thêm ticketRoutes
+
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/movies', movieRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/movies', movieRouter);
+app.use('/api/rooms', roomRoutes); // Sử dụng roomRoutes
+app.use('/api/showtimes', showtimeRoutes); // Sử dụng showtimeRoutes
+app.use('/api/tickets', ticketRoutes); // Sử dụng ticketRoutes
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
