ac5269d5fc7fbbbc22cad523d0b764c385b0008a
Chienlag1 <chienguyen17@gmail.com>
2025-06-23 22:56:38 +0700

    deebug6

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../app');
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app); // ✅ Export đúng format Vercel yêu cầu
